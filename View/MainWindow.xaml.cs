d7545a7dcd64f800526dcbd661cdb45799c5478a
cloudgh <Asus ROG@DESKTOP-EG4G4F7>
2023-11-12 21:30:52 +0700

    Добавьте файлы проекта.

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Map.ViewModel;
+
+namespace Map
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Инициализируйте вашу ViewModel и установите ее как контекст данных
+            MainViewModel viewModel = new MainViewModel();
+            DataContext = viewModel;
+        }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Mapsui.Layers;
+using Mapsui.Providers;
+using Mapsui.Styles;
+using Mapsui.UI.Wpf;
+using BruTile.Predefined;
+
+using BruTile.Web;
+using Mapsui.Tiling.Layers;
+
+namespace Map.ViewModel
+{
+    public class MainViewModel
+    {
+        public MapControl MapControl { get; }
+
+        public MainViewModel()
+        {
+            // Создаем карту
+            var map = new Mapsui.Map();
+
+            // Добавляем слой OpenStreetMap
+            var osmLayer = new TileLayer(
+                new HttpTileSource(new GlobalSphericalMercator(), "https://www.openstreetmap.org/{z}/{x}/{y}.png"))
+            {
+                Name = "OpenStreetMap"
+            };
+            map.Layers.Add(osmLayer);
+
+            // Инициализируем MapControl
+            MapControl = new MapControl { Map = map };
+        }
+    }
+}
